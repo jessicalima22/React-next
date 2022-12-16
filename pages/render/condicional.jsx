34204504df6ecc528ad94086d76db3fce611835e
jessicalima22 <jessica.lima22@hotmail.com>
2022-12-15 23:48:15 -0300

    condicional render exercise

---
+++ pages/render/condicional.jsx
@@ -0,0 +1,15 @@
+
+import SomentePar from "../../components/SomentePar"
+
+export default function condicional1(){
+    return(
+        <div>
+            <SomentePar numero = {0} /> 
+            <SomentePar numero = {1} /> 
+            <SomentePar numero = {2} /> 
+            <SomentePar numero = {3} /> 
+            <SomentePar numero = {4} /> 
+            <SomentePar numero = {5} /> 
+        </div>
+    )
+}
